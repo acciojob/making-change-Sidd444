1762f9fa60bd318abd3df5d7682876371c753962
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-13 14:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const makeChange = (c) => {
   // your name here
+  let quarter=0,dime=0,nickel=0,penny=0;
+  while(c>0){
+	  if(c>=25){
+		  c-=25;
+		  quarter++;
+	  }
+	  else if(c>=10){
+		  c-=10;
+		  nickel++;
+	  }else if(c>=5){
+		  c-=5;
+		  nickel++;
+	  }else{
+		  c-=1;
+		  penny++;
+	  }
+  }
+  const ans={"q":quarter,"d":dime,"n":nickel,"p":penny};
+  return ans;
 };
 
 // Do not the change the code below
